b765eccfcb06afa46070ffaca79c67aff0a67e87
KARTIK BHAGWANI <95032594+catrickcodings@users.noreply.github.com>
2024-10-17 19:24:55 +0530

    made searchfield

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
     super();
 
     this.state ={
-      monsters: []
+      monsters: [],
+      searchField:'',
 
       
     };
+    console.log('constructor')
   } //componentDidMount 
      componentDidMount(){
       console.log('component');
@@ -31,6 +33,9 @@ class App extends Component {
      }
   render (){
     console.log('render');
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+      });
     return (
     <div className="App">
       <input 
@@ -38,19 +43,17 @@ class App extends Component {
       type='search'
       placeholder='search monsters' 
       onChange={(event) => {
-        console.log({ startingArray: this.state.monsters }); 
-        const searchString = event.target.value.toLocaleLowerCase();
-        const filteredMonsters = this.state.monsters.filter((monster) => {
-        return monster.name.toLocaleLowerCase().includes(searchString);
-        });
+        
+        const searchField = event.target.value.toLocaleLowerCase();
+      
         this.setState(() =>  {
-          return { monsters: filteredMonsters};
-        })
+          return { searchField };
+        });
       }}
       />
       
       {
-     this.state.monsters.map((monster) => {
+       filteredMonsters.map((monster) => {
       return <div key={monster.id}> <h1>{monster.name}</h1></div>
      })}
      
